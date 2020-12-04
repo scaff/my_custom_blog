8f5551c572e7ccbf1116439c2229bb393e08e136
Thibault Roussel <thibaultroussel@MacBook-Pro-de-Thibault-2.local>
2020-12-04 13:51:48 +0100

    /post/id renvoie maintenant le titre de l'article s'il est trouvé

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,13 +10,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     /**
-     * @Route("/post", name="post")
+     * @Route("/post/{id}", name="post")
      */
-    public function index(): Response
+    public function index($id): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PostController.php',
-        ]);
+        $post = $this->getDoctrine() // on récupère Doctrine
+            ->getRepository(Post::class) // on récupère le répository correspondant au Post
+            ->find($id); // on récupère le post dont l'id correspond à l'id passé en URL
+
+        // si je n'ai pas d'article à cet id là
+        if(!$post) {
+            // alors je retourne "Pas d'article"
+            return new Response('Pas d\'article.');
+        }
+
+        // dans le cas contraire j'affiche l'article
+        return new Response('Mon article : <h1>'.$post->getTitle().'</h1>');
     }
 }
