64549b21a57ac0021e597f5f07cbf530d4c0f6ac
Thibault Roussel <thibaultroussel@MacBook-Pro-de-Thibault-2.local>
2020-12-04 14:54:11 +0100

    Changement de méthode (injection de dépendance) pour récupérer le repository

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,19 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\Post;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
     /**
      * @Route("/post/{id}", name="post")
      */
-    public function index($id): Response
+    public function index($id, PostRepository $postRepository): Response
     {
-        $post = $this->getDoctrine() // on récupère Doctrine
-            ->getRepository(Post::class) // on récupère le répository correspondant au Post
+        $post = $postRepository // on récupère le répository correspondant au Post
             ->find($id); // on récupère le post dont l'id correspond à l'id passé en URL
 
         // dans le cas contraire j'affiche l'article
